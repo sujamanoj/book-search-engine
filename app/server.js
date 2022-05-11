734feb1529eaa104dbee952352a6034549926d12
sujamanoj <suja@masstv.app>
2022-05-11 16:12:37 -0700

    add heroku

---
+++ app/server.js
@@ -0,0 +1,39 @@
+const { animals } = require("./data/animals.json");
+const express = require("express");
+const app = express();
+
+app.get("/api/animals", (req, res) => {
+  res.json(animals);
+});
+
+function filterByQuery(query, animalsArray) {
+  let filteredResults = animalsArray;
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animals) => animal.diet === query.diet
+    );
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animals) => animal.species === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animals) => animal.name === query.name
+    );
+  }
+  return filteredResults;
+}
+
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
